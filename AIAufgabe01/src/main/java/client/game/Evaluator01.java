03c703940781b60027529a0ca899c51d26a2b3e2
Jakob Warnow <jakob.warnow@gmx.de>
2015-05-16 10:12:51 +0200

    Erste Version vertig vom Evaluator

--- AIAufgabe01/src/main/java/client/game/Evaluator01.java
+++ AIAufgabe01/src/main/java/client/game/Evaluator01.java
@@ -4,18 +4,31 @@ public class Evaluator01
 {
 	private IBoard gameBoard;
 	private byte playerID = (byte) 255;
-	private byte valuation = (byte) 0;
+	private int valuation = 0;
 	
 	public Evaluator01(IBoard gameBoard, byte playerID)
 	{
 		this.gameBoard = gameBoard;
 		this.playerID = playerID;
 	}
-
 	
+	public byte calc()
+	{
+		
+		byte[][] Positions = this.gameBoard.GetPosition(this.playerID);
+		for (int i = 0; i < Positions.length; i++)
+		{
+			byte X = Positions[i][0];
+			byte Y = Positions[i][1];
+			if(gameBoard.GetField(X, Y) == 0);
+				this.valuation += 2;
+			
+		}
+		return (byte)this.valuation;
+	}
 	
 	public byte getValuation()
 	{
-		return valuation;
+		return (byte)this.valuation;
 	}
 }
