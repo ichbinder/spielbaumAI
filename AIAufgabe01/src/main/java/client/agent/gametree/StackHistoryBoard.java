b522b01ccb8f6b567ff102d7a3ecad1962fd50c6
Julian Löhr <julian.loehr@outlook.com>
2015-05-19 17:31:31 +0200

    Implemented the Move and Undo Move MEthods

--- AIAufgabe01/src/main/java/client/agent/gametree/StackHistoryBoard.java
+++ AIAufgabe01/src/main/java/client/agent/gametree/StackHistoryBoard.java
@@ -3,6 +3,7 @@ package client.agent.gametree;
 import java.util.ArrayList;
 
 import lenz.htw.kimpl.Move;
+import client.game.Board;
 import client.game.IBoard;
 
 public final class StackHistoryBoard implements IStackHistoryBoard {
@@ -25,9 +26,16 @@ public final class StackHistoryBoard implements IStackHistoryBoard {
 	private int StackPointer = 0;
 	
 	private int CurrentDirection = 0;
+	private Move MoveBuffer = null;	
+
+	private byte[][] Field = new byte[8][8];
+	private byte[][] Index = new byte[8][8];
+	private byte[][][] Position = new byte[4][6][2];
+	private boolean[] ActivePlayers = new boolean[4];
 	
-	public StackHistoryBoard()
+	public StackHistoryBoard()	
 	{
+		MoveBuffer = new Move(0,0,0,0);
 		Stack = new ArrayList<StackEntry>(InitialStackSize);
 		for(int i = 0; i < InitialStackSize; i++)
 		{
@@ -38,6 +46,14 @@ public final class StackHistoryBoard implements IStackHistoryBoard {
 	public final int GetStackSize()
 	{
 		return Stack.size();
+	}
+	
+	private final void CopyMove(Move Move)
+	{
+		MoveBuffer.fromX = Move.fromX;
+		MoveBuffer.fromY = Move.fromY;
+		MoveBuffer.toX = Move.toX;
+		MoveBuffer.toY = Move.toY;
 	}
 	
 	public final void Setup(IBoard CurrentBoard) {
@@ -80,35 +96,134 @@ public final class StackHistoryBoard implements IStackHistoryBoard {
 	}
 
 	public void DoMove(Move Move) {
+		CopyMove(Move);
 		//Rotate Move
-		
+		Board.RotateMove(MoveBuffer, CurrentDirection);
 		//Process Move
+		ProcessMove(Move);
 	}
 	
-	public void Forfeit() {	
+	private final void ProcessMove(Move Move)
+	{
+		StackEntry Entry = PushStack();
+		Entry.StackCommand = Command.Move;
+		Entry.FromX = (byte) Move.fromX;
+		Entry.FromY = (byte) Move.fromY;
+		Entry.ToX = (byte) Move.toX;
+		Entry.ToY = (byte) Move.toY;
+		
+		byte ToField = Field[Entry.ToX][Entry.ToY];
+
+		// Check if Piece and/or Player was defeated
+		if(ToField != -1)
+		{
+			Entry.TakenPiece = ToField;
+			byte ToIndex = Index[Entry.ToX][Entry.ToY];
+			Index[Entry.ToX][Entry.ToY] = -1;
+			Position[ToField][ToIndex][0] = -1;
+			Position[ToField][ToIndex][1] = -1;
+			
+			if(ActivePlayers[ToField])
+			{
+				boolean PieceLeft = false;
+				for(int i = 0; i < 6; i++)
+				{
+					if(Position[ToField][i][0] != -1)
+					{
+						PieceLeft = true;
+						break;
+					}
+				}
+				
+				if(!PieceLeft)
+				{
+					ActivePlayers[ToField] = false;
+					Entry.DefeatedPlayer = true;
+				}
+			}
+		}
+		else
+		{
+			Entry.TakenPiece = -1;
+		}
+		
+		// Make Move
+		byte FromField = Field[Entry.FromX][Entry.FromY];
+		Field[Entry.FromX][Entry.FromY] = -1;
+		byte FromIndex = Index[Entry.FromX][Entry.FromY];
+		
+		Field[Entry.ToX][Entry.ToY] = FromField;
+		Index[Entry.ToX][Entry.ToY] = FromIndex;
+		Position[FromField][FromIndex][0] = Entry.ToX;
+		Position[FromField][FromIndex][1] = Entry.ToY;	
+	}
+	
+	private final void UndoMove(StackEntry Entry)
+	{
+		//Move Piece Back
+		byte ToField = Field[Entry.ToX][Entry.ToY];
+		byte ToIndex = Index[Entry.ToX][Entry.ToY];
+		
+		Field[Entry.FromX][Entry.FromY] = ToField;
+		Index[Entry.FromX][Entry.FromY] = ToIndex;
+		Position[ToField][ToIndex][0] = Entry.FromX;
+		Position[ToField][ToIndex][1] = Entry.FromY;
+		
+		if(Entry.TakenPiece == -1)
+		{
+			Field[Entry.ToX][Entry.ToY] = -1;
+			Index[Entry.ToX][Entry.ToY] = -1;
+		}
+		else
+		{
+			Field[Entry.ToX][Entry.ToY] = Entry.TakenPiece;
+			byte PositionIndex = 0;
+			for(; PositionIndex < 6; PositionIndex++)
+			{
+				if(Position[Entry.TakenPiece][PositionIndex][0] == -1)
+				{
+					break;
+				}
+			}
+			
+			Index[Entry.ToX][Entry.ToY] = PositionIndex;
+			Position[Entry.TakenPiece][PositionIndex][0] = Entry.ToX;
+			Position[Entry.TakenPiece][PositionIndex][1] = Entry.ToY;
+			
+			if(Entry.DefeatedPlayer)
+			{
+				ActivePlayers[Entry.TakenPiece] = true;
+			}		
+		}		
+	}
+	
+	public final void Forfeit() {	
 		StackEntry Entry = PushStack();
 		Entry.StackCommand = Command.Forfeit;
+		Entry.TakenPiece = (byte) CurrentDirection;
 		
-		// TODO Update Active Players
+		// Update Active Players
+		ActivePlayers[Entry.TakenPiece] = false;
 	}
 	
-	public void RotateBoard() {
+	public final void RotateBoard() {
 		StackEntry Entry = PushStack();
 		Entry.StackCommand = Command.Rotate;
 		
 		CurrentDirection = (CurrentDirection + 1) % 4;
 	}
 
-	public void Pop() {
+	public final void Pop() {
 		
 		StackEntry Entry = PopStack();
 		
 		switch(Entry.StackCommand)
 		{
 		case Forfeit:
-			// TODO Update Active Players
+			ActivePlayers[Entry.TakenPiece] = true;
 			break;
 		case Move:
+			UndoMove(Entry);
 			break;
 		case Rotate:
 			CurrentDirection = (CurrentDirection + 3) % 4;
@@ -137,6 +252,10 @@ public final class StackHistoryBoard implements IStackHistoryBoard {
 	public byte[][] GetPiecePositions() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public final boolean IsCurrentPlayerActive() {
+		return ActivePlayers[CurrentDirection];
 	}
 
 }
